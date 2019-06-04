696fc5cc9a9db92d21a70f98ee7eb92c587d8683
raoweijian <raoweijian331@qq.com>
2019-06-05 00:25:35 +0800

    add xvideos support

--- app/task.py
+++ app/task.py
@@ -34,7 +34,6 @@ flask_celery = make_celery(flask_app)
 @flask_celery.task
 def download_video(task_id, path):
     task = Task.query.get(task_id)
-    task.status = 1
 
     headers = {
         'User-Agent':
@@ -44,26 +43,30 @@ def download_video(task_id, path):
     s = requests.Session()
     resp = s.get(task.url, headers=headers)
     html = etree.HTML(resp.content)
-    title = ''.join(html.xpath('//h1//text()')).strip()
+
+    site = "pornhub"
+    if task.url.startswith("https://www.xvideos.com"):
+        site = "xvideos"
+
+    # 获取视频标题
+    title = get_title(html, site)
+
+    # 更新任务标题和状态
     task.title = title
+    task.status = 1
     db.session.add(task)
     db.session.commit()
 
+    # 开始下载视频
+    download_url = get_download_url(html, site)
 
-    js = html.xpath('//*[@id="player"]/script/text()')[0]
-    tem = re.findall('var\\s+\\w+\\s+=\\s+(.*);\\s+var player_mp4_seek', js)[-1]
-    con = json.loads(tem)
-
-    for _dict in con['mediaDefinitions']:
-        if 'quality' in _dict.keys() and _dict.get('videoUrl'):
-            try:
-                download(path, _dict.get('videoUrl'), title, 'mp4')
-                task.status = 2
-                break
-            except Exception:
-                print("download exception")
-                task.status = -1
-                pass
+    try:
+        download(path, download_url, title, 'mp4')
+        task.status = 2
+    except Exception:
+        print("download exception")
+        task.status = -1
+        pass
 
     db.session.add(task)
     db.session.commit()
@@ -74,3 +77,22 @@ def download(path, url, name, filetype):
     if os.path.exists(filepath):
         return
     urllib.request.urlretrieve(url, '%s' % filepath)
+
+
+def get_download_url(html, site):
+    if site == "xvideos":
+        return html.cssselect("#html5video_base")[0].xpath("div//a")[-1].values()[1]
+    elif site == "pornhub":
+        js = html.xpath('//*[@id="player"]/script/text()')[0]
+        tem = re.findall('var\\s+\\w+\\s+=\\s+(.*);\\s+var player_mp4_seek', js)[-1]
+        con = json.loads(tem)
+        for _dict in con['mediaDefinitions']:
+            if 'quality' in _dict.keys() and _dict.get('videoUrl'):
+                return _dict.get('videoUrl')
+
+
+def get_title(html, site):
+    if site == "xvideos":
+        return html.cssselect("h2.page-title")[0].text.strip()
+    elif site == "pornhub":
+        return ''.join(html.xpath('//h1//text()')).strip()
