0cfecd0f15d744e3a79995000fc5d88a81ed7a58
Weijian Rao <raoweijian331@qq.com>
2019-12-21 04:38:57 +0800

    update

--- app/task.py
+++ app/task.py
@@ -3,9 +3,11 @@ import re
 import json
 import requests
 import urllib
+import js2py
 
 from celery import Celery
 from lxml import etree
+from clint.textui import progress
 
 from . import flask_app, db
 from .models import Task
@@ -29,19 +31,19 @@ def make_celery(flask_app):
 
 
 flask_celery = make_celery(flask_app)
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
+}
 
 
 @flask_celery.task
 def download_video(task_id, path):
     task = Task.query.get(task_id)
 
-    headers = {
-        'User-Agent':
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36',
-    }
-
-    s = requests.Session()
-    resp = s.get(task.url, headers=headers)
+    # s = requests.Session()
+    print("try to download %s" % task.url)
+    resp = requests.get(task.url, headers=headers, verify=False)
+    print("get base html done")
     html = etree.HTML(resp.content)
 
     site = "pornhub"
@@ -63,32 +65,40 @@ def download_video(task_id, path):
     try:
         download(path, download_url, title, 'mp4')
         task.status = 2
-    except Exception:
+        db.session.add(task)
+    except Exception as e:
+        print(e)
         print("download exception")
-        task.status = -1
+        # task.status = -1
+        db.session.delete(task)
         pass
-
-    db.session.add(task)
     db.session.commit()
-
-
-def download(path, url, name, filetype):
-    filepath = '%s/%s.%s' % (path, name, filetype)
-    if os.path.exists(filepath):
-        return
-    urllib.request.urlretrieve(url, '%s' % filepath)
 
 
 def get_download_url(html, site):
     if site == "xvideos":
         return html.cssselect("#html5video_base")[0].xpath("div//a")[-1].values()[1]
     elif site == "pornhub":
-        js = html.xpath('//*[@id="player"]/script/text()')[0]
-        tem = re.findall('var\\s+\\w+\\s+=\\s+(.*);\\s+var player_mp4_seek', js)[-1]
-        con = json.loads(tem)
-        for _dict in con['mediaDefinitions']:
-            if 'quality' in _dict.keys() and _dict.get('videoUrl'):
-                return _dict.get('videoUrl')
+        js_temp = html.xpath('//script/text()')
+        for j in js_temp:
+            if 'flashvars' in j:
+                js = ''.join(j.split('\n')[:-8])
+                videoUrl = exeJs(js)
+                return videoUrl
+
+
+def download(path, url, name, filetype):
+    filepath = '%s/%s.%s' % (path, name, filetype)
+    if os.path.exists(filepath):
+        return
+
+    response = requests.get(url, headers=headers, stream=True)
+    with open(filepath, "wb") as fp:
+        total_length = int(response.headers.get('content-length'))
+        for ch in progress.bar(response.iter_content(chunk_size=2391975), expected_size=(total_length / 1024) + 1):
+            if ch:
+                fp.write(ch)
+    print('download success :: %s' % filepath)
 
 
 def get_title(html, site):
@@ -96,3 +106,16 @@ def get_title(html, site):
         return html.cssselect("h2.page-title")[0].text.strip()
     elif site == "pornhub":
         return ''.join(html.xpath('//h1//text()')).strip()
+
+
+def exeJs(js):
+    flashvars = re.findall('flashvars_\d+', js)[0]
+    res = js2py.eval_js(js + flashvars)
+    if res.quality_720p:
+        return res.quality_720p
+    elif res.quality_480p:
+        return res.quality_480p
+    elif res.quality_240p:
+        return res.quality_240p
+    else:
+        print('parse url error')
